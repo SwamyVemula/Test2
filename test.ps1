a5b2b983691270768889bd10351edd779d6d882f
SwamyVemula <swamy.vemula87@gmail.com>
2018-11-05 14:57:22 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,6 @@
 New-Item -ItemType directory -Path C:\ices
+net start MSSQLSERVER
+sc config SQLBrowser start= auto
+sc config SQLSERVERAGENT start= auto
+net start SQLBrowser
+net start SQLSERVERAGENT
